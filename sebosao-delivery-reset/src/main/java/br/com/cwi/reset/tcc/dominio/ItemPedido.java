6b0ee24dba3d4f11e927578a9068f2584c92581b
Wanderson Herênio Aragão Leite <35743503+wando777@users.noreply.github.com>
2021-03-23 21:26:15 -0300

    Implementando security authenticator

---
+++ sebosao-delivery-reset/src/main/java/br/com/cwi/reset/tcc/dominio/ItemPedido.java
@@ -0,0 +1,41 @@
+package br.com.cwi.reset.tcc.dominio;
+
+import javax.persistence.*;
+
+@Entity
+public class ItemPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_produto")
+    private Produto produto;
+
+    private Integer quantidade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+}
